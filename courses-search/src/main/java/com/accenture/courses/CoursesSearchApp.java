4cc617fdca5ff19db6552f5e0401bc74d077c09c
Leonardo Guzman <leonardo.guzman@accenture.com>
2021-10-22 00:11:14 -0500

    add spring boot dependecies

---
+++ courses-search/src/main/java/com/accenture/courses/CoursesSearchApp.java
@@ -0,0 +1,17 @@
+package com.accenture.courses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+public class CoursesSearchApp
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(CoursesSearchApp.class, args);
+    }
+}
