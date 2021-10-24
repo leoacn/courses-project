0823a20e724c8256085d228482f8e3e62339ab52
Marcos Magaña Hernández <imarcosmaganah@gmail.com>
2021-10-23 21:15:33 -0500

    log

--- courses-view/src/main/java/com/accenture/courses/App.java
+++ courses-view/src/main/java/com/accenture/courses/App.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class App
 {
-    public static void main( String[] args )
-    {
+    public static void main( String[] args ) {
+        System.out.println("courses-view ok!");
         SpringApplication.run(App.class, args);
     }
 }
